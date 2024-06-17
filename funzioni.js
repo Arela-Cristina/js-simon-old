182dceecbca70c7be49cf60600dc9fbedb14ac32
Arela-Cristina <instagrandine@gmail.com>
2024-06-17 22:33:33 +0200

    finish

--- funzioni.js
+++ funzioni.js
@@ -2,3 +2,16 @@
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+//funzione che controlla
+function checkAnswer(array1, array2) {
+    let result = [];
+    for (let i = 0; i < 5; i++) {
+        if (array1.includes(array2[i])) {
+            result.push(array2[i]);
+
+        }
+        console.log(i);
+    }
+    return result;
+}
--- main.js
+++ main.js
@@ -4,6 +4,10 @@
 //Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+const userInput = document.getElementById('input');
+const button = document.getElementById('button');
+
+
 //Creiamo un array di 5 numeri casuali.
 const arrayNum = [];
 
@@ -16,16 +20,33 @@ let outputNumbers = document.getElementById('text');
 outputNumbers.append(arrayNum);
 console.log('Siamo i numeri random', arrayNum);
 
-//facciamo  sparire i numeri 
-outputNumbers.remove(arrayNum);
-
 
 //Da lì parte un timer di 30 secondi.
 setTimeout(myFunction, 3000);
+
+//facciamo  sparire i numeri 
 function myFunction() {
+    outputNumbers.remove();
     //e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-    for (i = 0; i < 5; i++) {
-        let userInput = prompt('Inserisci i numeri che hai visto nello schermo');
-    }
+    // for (i = 0; i < 5; i++) {
+    //     let userInput = prompt('Inserisci i numeri che hai visto nello schermo');
+    // }
 }
+
+let contatore = 0;
+console.log('Sono stati identificati ', contatore, ' numeri');
+
+let answerUser = [];
+
+button.addEventListener('click', function () {
+    let userNum = parseInt(userInput.value);
+    answerUser.push(userNum);
+    console.log('Sono il valore', answerUser);
+    if (answerUser.length === 5) {
+        let x = checkAnswer(arrayNum, answerUser);
+        console.log(x);
+    }
+});
+
+
 

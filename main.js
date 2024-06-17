9810017bfb36250cfb62e77f30ab0710941756ab
Arela-Cristina <instagrandine@gmail.com>
2024-06-17 17:17:17 +0200

    Add setTimeout and anonymous function

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 // Descrizione:
 //Visualizzare in pagina 5 numeri casuali.
-//Da lì parte un timer di 30 secondi.
+
 //Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-//Visualizzare in pagina 5 numeri casuali.
+//Creiamo un array di 5 numeri casuali.
 const arrayNum = [];
+
 for (let i = 0; i < 5; i++) {
     arrayNum.push(randomNumber(1, 100));
-    let outputNumbers = document.getElementById('text');
-    outputNumbers = arrayNum;
-
 }
 
+//Visualizzare in pagina 5 numeri casuali.
+let outputNumbers = document.getElementById('text');
+outputNumbers.append(arrayNum);
 console.log('Siamo i numeri random', arrayNum);
+
+//facciamo  sparire i numeri 
+outputNumbers.remove(arrayNum);
+
+
+//Da lì parte un timer di 30 secondi.
+setTimeout(myFunction, 3000);
+function myFunction() {
+    //e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    for (i = 0; i < 5; i++) {
+        let userInput = prompt('Inserisci i numeri che hai visto nello schermo');
+    }
+}
+
